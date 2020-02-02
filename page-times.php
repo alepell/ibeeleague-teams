c253d8974394cfb294ba2d80c0ab6a923963124b
Alexandre Pellegrino <alesirius@gmail.com>
2020-02-02 17:28:43 -0300

    fixed loop - names
    still working on types loop

--- page-times.php
+++ page-times.php
@@ -11,16 +11,25 @@
 
 <section class="times">
 
-    <?php while($times->have_posts()):$times->the_post(); ?>
+    <?php while($times->have_posts()):$times->the_post(); 
 
+      $membro1 = get_field('team_member_1');
+      $membro2 = get_field('team_member_2');
+      $membro3 = get_field('team_member_3');
+      $membro4 = get_field('team_member_4');
+      
+    ?>
       <ul>
-        <h3><?=  ?></h3>
-        <li></li>
-        <li></li>
-        <li></li>
-        <li></li>
-      </ul>
+          
+          <h3><?= the_title(); ?></h3>
+          <li><?= $membro1['nome'];?>     <?=$membro1['tipo']?></li>
+          <li><?= $membro2['nome'];?></li>
+          <li><?= $membro3['nome'];?></li>
+          <li><?= $membro4['nome'];?></li>
 
-    <?php endwhile; ?>
+        
+      </ul>
+      <br>
+    <?php endwhile;?>
 
 </section>
